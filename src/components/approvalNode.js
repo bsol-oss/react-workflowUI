49a602697b6fb7cf382a799b9253254064900808
Maxch3306 <127709614+Maxch3306@users.noreply.github.com>
2023-06-08 16:19:47 +0800

    update

--- src/components/approvalNode.js
+++ src/components/approvalNode.js
@@ -1,5 +1,11 @@
 import React, { memo } from "react";
-import { Handle, Position } from "reactflow";
+import { 
+  Handle, 
+  Position
+} from "reactflow";
+import { 
+  ChakraBaseProvider
+} from '@chakra-ui/react'
 
 export default memo(({ data, isConnectable }) => {
 
@@ -20,88 +26,91 @@ export default memo(({ data, isConnectable }) => {
     data.name = event.target.value;
     setTaskName(event.target.value);
   }
+
   return (
-    <div >
-      <Handle
-        type="target"
-        position={Position.Top}
-        style={{ background: "#555", width: "15px", height: '15px', marginTop: '-10px', borderRadius: '0px' }}
-        onConnect={(params) => console.log("handle onConnect", params)}
-        isConnectable={isConnectable}
-      />
-      <div
-        style={{
-          width: "250px",
-          display: "flex",
-          flexDirection: "column",
-          gap: "5px",
-          alignItems: "center"
-        }}
-      >
-        <div style={{ width: "200px" }}>
-          <label>Task ID:</label>
-          <input value={data.taskId} disabled style={{ width: '192px' }} />
-        </div>
-        <div style={{ width: "200px", paddingBottom: '10px' }}>
-          <label>Name:</label>
-          <input value={taskname} style={{ width: '192px' }} onChange={(e) => { handleNameChange(e)}} />
-        </div>
-        <div style={{ width: "200px" }}>
-          <label>Method:</label>
-          <select value={method} style={{ width: "200px" }} onChange={(e) => handleChange(e)} >
-            <option value=''></option>
-            <option value='ALL'>ALL</option>
-            <option value='ONE'>ONE</option>
-            <option value='MAJORITY'>MAJORITY</option>
-            <option value='VETTING'>VETTING</option>
-          </select>
-        </div>
-        <div style={{ width: "200px", paddingBottom: '10px' }}>
-          <label>Approver:</label>
-          <select value={approver} style={{ width: '200px' }} onChange={(e) => { handleApproverChange(e)}} >
-            <option  value=""></option>
-            {
-              data.approverOpt.map((item, index) => {
-                return <option key={index} value={item}>{item}</option>
-              })
-            }
-          </select>
-        </div>
-        {
-          <div
-            style={{
-              width: "200px",
-              display: "flex",
-              justifyContent: "space-between"
-            }}
-          >
-            <div style={{ border: "1px solid #555", width: "90px",textAlign:"center" }}>
-              Approved
-            </div>
-            <div style={{ border: "1px solid #555", width: "90px",textAlign:"center" }}>
-              Rejected
-            </div>
+    <ChakraBaseProvider>
+      <div style={{borderRadius:'10px',border:'1px solid',background:'#fff',padding:'10px'}}>
+        <Handle
+          type="target"
+          position={Position.Top}
+          style={{ background: "#555", width: "15px", height: '15px', marginTop: '-10px', borderRadius: '0px' }}
+          onConnect={(params) => console.log("handle onConnect", params)}
+          isConnectable={isConnectable}
+        />
+        <div
+          style={{
+            width: "250px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "5px",
+            alignItems: "center"
+          }}
+        >
+          <div style={{ width: "200px" }}>
+            <label>Task ID:</label>
+            <input type="text" value={data.taskId} disabled style={{ padding: '0px 0px 0px 5px',width: '200px',height:'25px',borderRadius:'5px',border: "1px solid #555" }} />
           </div>
-        }
+          <div style={{ width: "200px" }}>
+            <label>Name:</label>
+            <input type="text" value={taskname} style={{padding: '0px 0px 0px 5px', width: '200px',height:'25px',borderRadius:'5px',border: "1px solid #555" }} onChange={(e) => { handleNameChange(e)}} />
+          </div>
+          <div style={{ width: "200px" }}>
+            <label>Method:</label>
+            <select value={method} style={{ width: "200px",height:'25px',borderRadius:'5px',border: "1px solid #555" }} onChange={(e) => handleChange(e)} >
+              <option value=""></option>
+              <option value="ALL">ALL</option>
+              <option value="ONE">ONE</option>
+              <option value="MAJORITY">MAJORITY</option>
+              <option value="VETTING">VETTING</option>
+            </select>
+          </div>
+          <div style={{ width: "200px", paddingBottom: '10px' }}>
+            <label>Approver:</label>
+            <select  value={approver} style={{ width: '200px',height:'25px',borderRadius:'5px',border: "1px solid #555" }} onChange={(e) => { handleApproverChange(e)}} >
+              <option value=""></option>
+              {
+                data.approverOpt.map((item, index) => {
+                  return <option key={index} value={item}>{item}</option>
+                })
+              }
+            </select>
+          </div>
+          {
+            <div
+              style={{
+                width: "200px",
+                display: "flex",
+                justifyContent: "space-between"
+              }}
+            >
+              <div style={{ borderRadius:'5px',border: "1px solid #555", width: "90px",textAlign:"center" }}>
+                Approved
+              </div>
+              <div style={{ borderRadius:'5px',border: "1px solid #555", width: "90px",textAlign:"center" }}>
+                Rejected
+              </div>
+            </div>
+          }
+        </div>
+
+        <Handle
+          type="source"
+          position={Position.Bottom}
+          id="a"
+          style={{ background: "#555", width: "15px", height: '15px', marginBottom: '-8px', Bottom: 10, left: 90, borderRadius: '0px' }}
+          isConnectable={isConnectable}
+        />
+
+
+        <Handle
+          type="source"
+          position={Position.Bottom}
+          id="b"
+          style={{ background: "#555", width: "15px", height: '15px', marginBottom: '-8px', Bottom: 10, left: 180, borderRadius: '0px' }}
+          isConnectable={isConnectable}
+        />
+
       </div>
-
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        id="a"
-        style={{ background: "#555", width: "15px", height: '15px', marginBottom: '-8px', Bottom: 10, left: 90, borderRadius: '0px' }}
-        isConnectable={isConnectable}
-      />
-
-
-      <Handle
-        type="source"
-        position={Position.Bottom}
-        id="b"
-        style={{ background: "#555", width: "15px", height: '15px', marginBottom: '-8px', Bottom: 10, left: 180, borderRadius: '0px' }}
-        isConnectable={isConnectable}
-      />
-
-    </div>
+    </ChakraBaseProvider>
   );
 });
