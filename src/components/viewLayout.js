91e36662f619ca5e51d1c09142f7551b6f3c3da6
Maxch3306 <127709614+Maxch3306@users.noreply.github.com>
2023-10-11 16:52:28 +0800

    Update viewLayout.js

--- src/components/viewLayout.js
+++ src/components/viewLayout.js
@@ -190,7 +190,7 @@ const ViewLayoutFlow = (workflowJson) => {
     if(status === 'REJECTED'){
       return "#edcece";
     }
-    return "#8f8f8f";
+    return "#fffb80";
   }
 
   return (
@@ -220,10 +220,12 @@ const ViewLayoutFlow = (workflowJson) => {
                     }}>
                 <button style={{position:'absolute',right:'5px',top:'5px',width:'50px',height:'25px',color:'white',backgroundColor:'#4c4cef',border:'0px',borderRadius:'5px'}} onClick={()=>{setdataForDetail(null)}}>close</button>
                 <div style={{padding:'0px 3%'}}>
-                  <div>Task ID: {dataForDetail?.taskId}</div>
-                  <div>Name: {dataForDetail?.name}</div>
-                  <div>Method: {dataForDetail?.method}</div>
-                  <div>Approver:</div>
+                  {
+                  //<div>Task ID: {dataForDetail?.taskId}</div>
+                  }
+                  <div style={{whiteSpace:'nowrap'}}>Name: {dataForDetail?.name}</div>
+                  <div style={{whiteSpace:'nowrap'}}>Method: {dataForDetail?.method}</div>
+                  <div style={{whiteSpace:'nowrap'}}>Approver:</div>
                   {
                     //dataForDetail?.approver[0].name === undefined &&
                     //<>
@@ -237,48 +239,48 @@ const ViewLayoutFlow = (workflowJson) => {
                   }
                 </div>
                 <div style={{padding:'0px 3%',display:'flex',borderRadius:'10px',flexDirection:'row',gap:'10px',flexWrap:'wrap',justifyContent:'center' }}>
-                { 
-                dataForDetail?.approver &&
-                dataForDetail?.approver != undefined &&
-                  (typeof(dataForDetail?.approver) !== 'string') &&
-                    dataForDetail?.approver.map((item)=>{
-                        return(<>
-                        {
-                            typeof(item) !== 'string' &&
-                            <div style={{
-                                backgroundColor: setColor(item.status),
-                                border:'1px solid #bababa',
-                                padding:'10px',
-                                width:'300px',
-                                minHeight:'200px',
-                                display:'flex',
-                                borderRadius:'10px',
-                                flexDirection:'column',
-                                gap:'5px' 
-                              }}>
-                                <div>Name: {item.name}</div>
-                                <div>Email: {item.email}</div>
-                                <div>Status: {item.status}</div>
-                                <div>Comment:</div>
-                                <div style={{padding:'5px',display:'flex',borderRadius:'10px',flexDirection:'row',gap:'10px',flexWrap:'wrap',justifyContent:'center' }}>
-                                  {
-                                    item.comment.map((comment)=>{
-                                        return(
-                                          <div key={comment.name} style={{border:'1px solid #bababa',padding:'5px',display:'flex',borderRadius:'10px',flexDirection:'column',minWidth:'120px',maxWidth:'120px' ,backgroundColor:'white'}}>
-                                              <label>Doc:</label>
-                                              <div style={{whiteSpace:'nowrap',textOverflow:'ellipsis',maxWidth:'120px',overflowX:'hidden'}}>{comment.name}</div>
-                                              <label>Comment:</label>
-                                              <div style={{whiteSpace:'normal'}}>{comment.comment}</div>
-                                          </div>
-                                        )
-                                    })
-                                  }
-                                </div>
-                            </div>
-                        }
-                        </>)
-                    })
-                }
+                  { 
+                  dataForDetail?.approver &&
+                  dataForDetail?.approver != undefined &&
+                    (typeof(dataForDetail?.approver) !== 'string') &&
+                      dataForDetail?.approver.map((item)=>{
+                          return(<>
+                          {
+                              typeof(item) !== 'string' &&
+                              <div style={{
+                                  backgroundColor: setColor(item.status),
+                                  border:'1px solid #bababa',
+                                  padding:'10px',
+                                  width:'300px',
+                                  minHeight:'200px',
+                                  display:'flex',
+                                  borderRadius:'10px',
+                                  flexDirection:'column',
+                                  gap:'5px' 
+                                }}>
+                                  <div>Name: {item.name}</div>
+                                  <div>Email: {item.email}</div>
+                                  <div>Status: {item.status}</div>
+                                  <div>Comment:</div>
+                                  <div style={{padding:'5px',display:'flex',borderRadius:'10px',flexDirection:'row',gap:'10px',flexWrap:'wrap',justifyContent:'center' }}>
+                                    {
+                                      item.comment.map((comment)=>{
+                                          return(
+                                            <div key={comment.name} style={{border:'1px solid #bababa',padding:'5px',display:'flex',borderRadius:'10px',flexDirection:'column',minWidth:'120px',maxWidth:'120px' ,backgroundColor:'white'}}>
+                                                <label>Doc:</label>
+                                                <div style={{whiteSpace:'nowrap',textOverflow:'ellipsis',maxWidth:'120px',overflowX:'hidden'}}>{comment.name}</div>
+                                                <label>Comment:</label>
+                                                <div style={{whiteSpace:'normal'}}>{comment.comment}</div>
+                                            </div>
+                                          )
+                                      })
+                                    }
+                                  </div>
+                              </div>
+                          }
+                          </>)
+                      })
+                  }
                 </div>
             </div>
         }
